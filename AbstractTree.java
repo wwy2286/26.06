54c6f2d633722823d50a921d9a2ea03afac0965d
William <wwy2286@gmail.com>
2017-11-09 17:33:25 -0500

    1st commit

---
+++ AbstractTree.java
@@ -0,0 +1,17 @@
+public abstract class AbstractTree<E> implements Tree<E> {
+
+    public void inorder(){
+
+    }
+
+    public void postorder(){
+
+    }
+
+    public void preorder(){
+
+    }
+public boolean isEmpty(){
+        return getSize()==0;
+}
+}
